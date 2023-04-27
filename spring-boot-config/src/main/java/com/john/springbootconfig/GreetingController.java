d8df986f4dae0908ace1e17d252c30596449c986
JohnYHMM <johnyehtetmgmg@gmail.com>
2023-04-27 22:15:20 +0630

    Add Environment object

--- spring-boot-config/src/main/java/com/john/springbootconfig/GreetingController.java
+++ spring-boot-config/src/main/java/com/john/springbootconfig/GreetingController.java
@@ -2,6 +2,7 @@ package com.john.springbootconfig;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,8 +23,16 @@ public class GreetingController {
     @Autowired
     private DbSettings dbSettings;
 
+    @Autowired
+    Environment env;
+
     @GetMapping("/greeting")
     public String greeting() {
         return greetingMessage + staticMsg + list + "<br />" + dbSettings.toString();
+    }
+
+    @GetMapping("/envdetails")
+    public String envDetials(){
+        return env.toString();
     }
 }
