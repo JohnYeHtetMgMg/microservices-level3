914a10fb89f4393f9e3697e55b2d658a7b92997d
JohnYHMM <johnyehtetmgmg@gmail.com>
2023-04-28 22:56:08 +0630

    Add refresh scope to update configs at runtime for greeting controller

--- spring-boot-config/src/main/java/com/john/springbootconfig/GreetingController.java
+++ spring-boot-config/src/main/java/com/john/springbootconfig/GreetingController.java
@@ -2,6 +2,7 @@ package com.john.springbootconfig;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@RefreshScope
 public class GreetingController {
 
     @Value("${my.greeting: default value}")
